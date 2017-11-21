b0622e54ae2280aedbcddfa1539b6019c6ec3246
Jasper van Riet <jaspervanriet@gmail.com>
2017-11-21 17:34:02 +0100

    Add EventQueue

---
+++ distributed/systems/das/EventQueue.java
@@ -0,0 +1,34 @@
+package distributed.systems.das;
+
+import java.util.Iterator;
+import java.util.concurrent.PriorityBlockingQueue;
+
+/**
+ * Queue for all events
+ */
+public class EventQueue implements Iterable<Event> {
+
+	private PriorityBlockingQueue<Event> events = new PriorityBlockingQueue<Event> (11);
+
+	public synchronized boolean add (Event event) {
+		return this.events.add (event);
+	}
+
+	public synchronized void clear () {
+		this.events.clear ();
+	}
+
+	public synchronized boolean addAll (EventQueue queue) {
+		return this.events.addAll (queue.getEvents ());
+	}
+
+	public synchronized PriorityBlockingQueue<Event> getEvents () {
+		return this.events;
+	}
+
+	@Override
+	public Iterator<Event> iterator () {
+		return this.events.iterator ();
+	}
+
+}
