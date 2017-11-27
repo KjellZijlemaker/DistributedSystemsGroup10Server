c256a769ab6d60dbfe0ce5c198c419ed361ca0e0
Jasper van Riet <jaspervanriet@gmail.com>
2017-11-27 14:34:25 +0100

    Remove useless comment

--- distributed/systems/das/util/Log.java
+++ distributed/systems/das/util/Log.java
@@ -2,9 +2,6 @@ package distributed.systems.das.util;
 
 import java.util.logging.Logger;
 
-/**
- * Created by Jasper van Riet
- */
 public class Log {
 
 	public static void throwException (Exception e, Class c) {
