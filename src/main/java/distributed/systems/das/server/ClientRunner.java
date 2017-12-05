890a34320808e28c67ac9e57d06095f680fc5b48
Gil Wiraatmadja <gilwiraatmadja@gmail.com>
2017-12-05 21:02:05 +0100

    fix auto move

--- src/main/java/distributed/systems/das/server/ClientRunner.java
+++ src/main/java/distributed/systems/das/server/ClientRunner.java
@@ -5,6 +5,7 @@ import distributed.systems.das.server.Interfaces.RMIUserInterface;
 import distributed.systems.das.server.Services.Callback;
 import distributed.systems.das.server.Services.HeartbeatService;
 import distributed.systems.das.server.State.BattleField;
+import distributed.systems.das.server.Units.Dragon;
 import distributed.systems.das.server.Units.Player;
 import distributed.systems.das.server.Units.Unit;
 import distributed.systems.das.server.events.*;
@@ -19,7 +20,9 @@ import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.rmi.server.UnicastRemoteObject;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.Map;
 import java.util.Random;
 import java.util.UUID;
@@ -60,11 +63,12 @@ public class ClientRunner extends UnicastRemoteObject implements IMessageReceive
             end = (long) (currentTime + botTime);
         }
 
-        Unit localPlayer = new Player(10, 10, playerID);
+//        Unit localPlayer = new Player(10, 10, playerID);
 
 
         Callback updateClient = new Callback();
         try {
+            System.out.println("playerID: "+playerID);
 
             ClientRunner runner = new ClientRunner();
             String serverID = "123";
@@ -74,53 +78,176 @@ public class ClientRunner extends UnicastRemoteObject implements IMessageReceive
             remoteRegistry.bind(playerID, runner);
 
             Message m = new Message(0, System.currentTimeMillis(), playerID, Message.LOGIN);
-            server.onMessageReceived(m);
+            Message loginResp = server.onMessageReceived(m);
+            Unit u = (Unit)loginResp.body.get("unit");
+            Player p = new Player(u.getMaxHitPoints(), u.getAttackPoints(), u.getUnitID());
+            BattleField battlefield = (BattleField)loginResp.body.get("battlefield");
 
-            m = new Message(1, System.currentTimeMillis(), playerID, Message.MOVE);
-            m.body.put("x", 1);
-            m.body.put("y", 1);
-            server.onMessageReceived(m);
-
-            runner.startHeartbeat(server, playerID);
-
-            // Get values from server
-//            boolean gameState = initialStateBattlefield.getValue0();
-//            BattleField localBattlefield = initialStateBattlefield.getValue1();
-//            localPlayer = initialStateBattlefield.getValue2(); // Update local player from server
-//
-//            // If game is running:
-//            if (gameState) {
-////                System.out.println(localBattlefield.getUnits());
-//                System.out.println(localPlayer.getUnitID());
-//                System.out.println(localBattlefield.getMap());
-//            }
-//
-//            while (System.currentTimeMillis() < end) {
-//                Thread.sleep(1000);
-//
-//                for (int i = 0; i < localBattlefield.getUnits().size(); i++) {
-//                    System.out.println(localBattlefield.getUnits().get(i).getX());
-//                    System.out.println(localBattlefield.getUnits().get(i).getY());
-//                }
-//                String returnMessage = user.registerWish(localPlayer, new Move.MoveBuilder(1)
-//                        .setActor_id(localPlayer.getUnitID())
-//                        .setTargetX(1)
-//                        .setTargetY(1)
-//                        .createEvent());
-//                if (returnMessage.contains("OK")) {
-//                    if(updateClient.getEventlist() != null){
-//
-//                        // TODO Update client movement on local board
-//                    }
-//                }
-//            }
-//            user.disconnectUser(localPlayer); // Disconnect when lifespan of bot is up
+            runner.startSimulation(server, p, battlefield);
+            
+            //runner.startHeartbeat(server, playerID);
 
         } catch (Exception e) {
             e.printStackTrace();
             System.exit(2);
         }
 
+    }
+    
+    private boolean findAndHeal(IMessageReceivedHandler server, Player p, BattleField battlefield) throws Exception {	
+    	int nx = p.getX();
+    	int ny = p.getY();
+    	
+    	int tx = -1;
+		int ty = -1;
+		
+		boolean found = false;
+
+    	for (int i=-5; i<=5; i++) {
+    		for (int j=-5; j<=5; j++) {
+    			if (Math.abs(i) + Math.abs(j) > 5) {
+    				continue;
+    			}
+    			
+    			tx = nx + i;
+    			ty = ny + j;
+
+    			if (!battlefield.isLegalCoordinates(tx, ty)) {
+    				continue;
+    			}
+    			
+    			Unit u = battlefield.getUnit(tx, ty);
+    			
+    			if (!(u instanceof Player)) {
+    				continue;
+    			}
+    			
+    			Player tp = (Player)u;
+    			if (tp.getHitPoints() <= tp.getMaxHitPoints() / 2) {
+    				Message m = new Message(1, System.currentTimeMillis(), p.getUnitID(), Message.HEAL);
+    				m.body.put("x", tx);
+    				m.body.put("y", ty);
+    				m.body.put("healed", p.getAttackPoints());
+    					
+    				Message resp_m = server.onMessageReceived(m);
+    				battlefield.heal(resp_m);
+    				
+    				found = true;
+        			break;
+    			}
+    		}
+    		
+    		if (found) {
+    			break;
+    		}
+    	}
+    
+    	return found;
+    }
+    
+    private boolean directlyAttack(IMessageReceivedHandler server, Player p, BattleField battlefield) throws Exception {
+    	int nx = p.getX();
+    	int ny = p.getY();
+    	
+    	int tx = -1;
+		int ty = -1;
+		
+		boolean found = false;
+
+    	for (int i=-2; i<=2; i++) {
+    		for (int j=-2; j<=2; j++) {
+    			if (Math.abs(i) + Math.abs(j) > 2) {
+    				continue;
+    			}
+    			
+    			tx = nx + i;
+    			ty = ny + j;
+
+    			if (!battlefield.isLegalCoordinates(tx, ty)) {
+    				continue;
+    			}
+
+    			Unit u = battlefield.getUnit(tx, ty);
+    			
+    			if (!(u instanceof Dragon)) {
+    				continue;
+    			}
+    			
+    			Dragon td = (Dragon)u;
+
+    			Message m = new Message(1, System.currentTimeMillis(), p.getUnitID(), Message.ATTACK);
+    			m.body.put("x", tx);
+    			m.body.put("y", ty);
+    			m.body.put("damage", p.getAttackPoints()); // TODO: check if server use this name "ap"
+    					
+    			Message resp_m = server.onMessageReceived(m);
+    			battlefield.attack(resp_m);
+    			
+    			found = true;
+    			break;
+    		}
+    		
+    		if (found) {
+    			break;
+    		}
+    	}
+    	
+    	return found;
+    }
+
+    private boolean findNearestDragonAndMove(IMessageReceivedHandler server, Player p, BattleField battlefield) throws Exception {
+    	int nx = p.getX();
+    	int ny = p.getY();
+    	
+    	int tx = -1;
+		int ty = -1;
+
+		ArrayList<String> AliveDragons=new ArrayList<String>();
+		
+    	for (int i=0; i<BattleField.MAP_WIDTH; i++) {
+    		for (int j=0; j<BattleField.MAP_HEIGHT; j++) {
+	
+				Unit u = battlefield.getUnit(i, j);
+				if (!(u instanceof Dragon)) {
+					continue;
+				}
+
+				int distance = (Math.abs(nx-i) + Math.abs(ny-j));
+				AliveDragons.add(distance+","+tx+","+ty);
+
+    		}
+    	}
+
+    	Collections.sort(AliveDragons);
+    	
+    	//TODO: implement move
+		Message m = new Message(1, System.currentTimeMillis(), p.getUnitID(), Message.MOVE);
+		m.body.put("x", tx);
+		m.body.put("y", ty);
+				
+		Message resp_m = server.onMessageReceived(m);
+		battlefield.move(resp_m);
+    	
+    	return (!AliveDragons.isEmpty());
+    }
+    
+    
+    private void startSimulation(IMessageReceivedHandler server, Player p, BattleField battlefield) {
+    	Random random = new Random();
+    	
+    	try {
+	    	while (true) {	    		
+	    	    
+	    	    //TODO: test each action
+	    		boolean action = findAndHeal(server, p, battlefield)
+	    			|| directlyAttack(server, p, battlefield)
+	    			|| findNearestDragonAndMove(server, p, battlefield);
+	    		
+	    		Thread.sleep(1000);
+	    	}
+    	} catch (Exception e) {
+    		e.printStackTrace();
+    	}
     }
 
     @Override
@@ -133,7 +260,7 @@ public class ClientRunner extends UnicastRemoteObject implements IMessageReceive
         Message m = new Message(2, System.currentTimeMillis(), playerID, Message.HEARTBEAT);
         while (true) {
             server.onMessageReceived(m);
-            Thread.sleep(10);
+            Thread.sleep(1000);
         }
     }
 }
